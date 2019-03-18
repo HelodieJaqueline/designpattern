d5abda55808a7c0d5d7c21e0e4fb9fa7674bfc28
HelodieJaqueline <1037102215@qq.com>
2019-03-19 00:30:58 +0800

    prototype

---
+++ src/main/java/com/zhangrui/designpatterns/prototype/simple/ConcretePrototypeB.java
@@ -0,0 +1,12 @@
+package com.zhangrui.designpatterns.prototype.simple;
+
+/**
+ * Created by Tom.
+ */
+public class ConcretePrototypeB implements Prototype {
+
+    @Override
+    public Prototype clone() {
+        return null;
+    }
+}
---
+++ src/main/java/com/zhangrui/designpatterns/prototype/simple/Prototype.java
@@ -0,0 +1,8 @@
+package com.zhangrui.designpatterns.prototype.simple;
+
+/**
+ * Created by Tom.
+ */
+public interface Prototype{
+    Prototype clone();
+}
