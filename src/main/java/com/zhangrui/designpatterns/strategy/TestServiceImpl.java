b48d1eb58af51ea6ef68ccbe185deb1689e827f5
HelodieJaqueline <1037102215@qq.com>
2019-05-09 00:14:26 +0800

    strategy

---
+++ src/main/java/com/zhangrui/designpatterns/strategy/TestServiceImpl.java
@@ -0,0 +1,38 @@
+package com.zhangrui.designpatterns.strategy;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: ZhangRui
+ * @Date: Created at 2019-02-25-20:02
+ * @Description:
+ * @Modified: By
+ */
+@Service
+public class TestServiceImpl implements ITestService {
+
+	private static final String OPEN = "0";
+	private static final String CANCEL = "1";
+
+
+	@Override
+	public void testStrategy(OpenRegisterParam request) throws Exception {
+		getRegister(request.getState()).doRegister(request);
+	}
+
+	private IRegister getRegister(String state) throws Exception {
+		return SpringBeanProxy.getBean(IRegister.class, getRegisterBeanName(state));
+	}
+
+	private String getRegisterBeanName(String state) throws Exception {
+		switch (state) {
+			case OPEN:
+				return "openRegister";
+			case CANCEL:
+				return "canCelRegister";
+			default:
+				throw new Exception("状态不对");
+		}
+	}
+
+}
